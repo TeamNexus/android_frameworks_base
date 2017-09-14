4c1283e63bf94284288d41e2a2c46c0be1b8c49b
Lukas0610 <mail@lukasberger.at>
2017-09-14 21:39:03 +0200

    doze: allow to select sleep or doze depending on Ambient Display/Always-On-Display
    Change-Id: Icf59a5609c9c87aa8ee51e9b762bb09c3cec2d9b

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -226,6 +226,7 @@ import android.view.autofill.AutofillManagerInternal;
 import android.view.inputmethod.InputMethodManagerInternal;
 
 import com.android.internal.R;
+import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.policy.IKeyguardDismissCallback;
 import com.android.internal.policy.IShortcutService;
@@ -586,6 +587,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mHasSoftInput = false;
     boolean mTranslucentDecorEnabled = true;
     boolean mUseTvRouting;
+    boolean mDozeOnlyOnAmbientOrAlwaysOn;
 
     private boolean mHandleVolumeKeysInWM;
 
@@ -1410,7 +1412,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         } else if (count == 3) {
             powerMultiPressAction(eventTime, interactive, mTriplePressOnPowerBehavior);
         } else if (interactive && !mBeganFromNonInteractive) {
-            switch (mShortPressOnPowerBehavior) {
+            int behavior = mShortPressOnPowerBehavior;
+
+            if (mDozeOnlyOnAmbientOrAlwaysOn) {
+                if (new AmbientDisplayConfiguration(mContext).enabledForCurrentUser()) {
+                    behavior = SHORT_PRESS_POWER_GO_TO_SLEEP;
+                } else {
+                    behavior = SHORT_PRESS_POWER_REALLY_GO_TO_SLEEP;
+                }
+            }
+
+            switch (behavior) {
                 case SHORT_PRESS_POWER_NOTHING:
                     break;
                 case SHORT_PRESS_POWER_GO_TO_SLEEP:
@@ -1979,6 +1991,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mHandleVolumeKeysInWM = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_handleVolumeKeysInWindowManager);
+
+        mDozeOnlyOnAmbientOrAlwaysOn = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_dozeOnlyOnAmbientOrAlwaysOn);
 
         readConfigurationDependentBehaviors();
 
